8b6d21fcafe3f2ec78e1ccb2c7441f11b5cec756
Si Yu Lew <156303901+bananapielearnsjava@users.noreply.github.com>
2025-10-21 00:24:08 +0800

    Update support in JsonAdaptedPerson

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -176,9 +176,6 @@ class JsonAdaptedPerson {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
                     AlcoholicRecord.class.getSimpleName()));
         }
-        if (!AlcoholicRecord.isValidAlcoholicRecord(alcoholicRecord)) {
-            throw new IllegalValueException(AlcoholicRecord.MESSAGE_CONSTRAINTS);
-        }
         final AlcoholicRecord modelAlcoholicRecord = new AlcoholicRecord(alcoholicRecord);
 
         if (gender == null) {
