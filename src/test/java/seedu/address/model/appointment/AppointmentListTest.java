90ca1c480e0391e93cba4543da1f12969a8831b5
SoyWater <153174763+SoyWater@users.noreply.github.com>
2025-10-22 17:38:37 +0800

    Add tests for appointmentList

--- src/test/java/seedu/address/model/appointment/AppointmentListTest.java
+++ src/test/java/seedu/address/model/appointment/AppointmentListTest.java
@@ -1,4 +1,173 @@
 package seedu.address.model.appointment;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.testutil.TypicalAppointments.APPT_ALICE;
+import static seedu.address.testutil.TypicalAppointments.APPT_BENSON;
+import static seedu.address.testutil.TypicalAppointments.getTypicalAppointments;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.appointment.exceptions.DuplicateAppointmentException;
+import seedu.address.model.appointment.exceptions.AppointmentNotFoundException;
+import seedu.address.testutil.AppointmentBuilder;
+
 public class AppointmentListTest {
+
+    private final AppointmentList appointmentList = new AppointmentList();
+
+    @Test
+    public void contains_nullAppointment_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> appointmentList.contains(null));
+    }
+
+    @Test
+    public void contains_appointmentNotInList_returnsFalse() {
+        assertFalse(appointmentList.contains(APPT_ALICE));
+    }
+
+    @Test
+    public void contains_appointmentInList_returnsTrue() {
+        appointmentList.add(APPT_ALICE);
+        assertTrue(appointmentList.contains(APPT_ALICE));
+    }
+
+    @Test
+    public void contains_appointmentWithSameIdentityFieldsInList_returnsTrue() {
+        appointmentList.add(APPT_ALICE);
+        Appointment editedAlice = new AppointmentBuilder(APPT_ALICE)
+                .withNotes("Changed notes")
+                .build();
+        assertTrue(appointmentList.contains(editedAlice));
+    }
+
+    @Test
+    public void add_nullAppointment_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> appointmentList.add(null));
+    }
+
+    @Test
+    public void add_duplicateAppointment_throwsDuplicateAppointmentException() {
+        appointmentList.add(APPT_ALICE);
+        assertThrows(DuplicateAppointmentException.class, () -> appointmentList.add(APPT_ALICE));
+    }
+
+    @Test
+    public void setAppointment_nullTargetAppointment_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> appointmentList.setAppointment(null, APPT_ALICE));
+    }
+
+    @Test
+    public void setAppointment_nullEditedAppointment_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> appointmentList.setAppointment(APPT_ALICE, null));
+    }
+
+    @Test
+    public void setAppointment_targetAppointmentNotInList_throwsAppointmentNotFoundException() {
+        assertThrows(AppointmentNotFoundException.class, () -> appointmentList.setAppointment(APPT_ALICE, APPT_ALICE));
+    }
+
+    @Test
+    public void setAppointment_editedAppointmentIsSameAppointment_success() {
+        appointmentList.add(APPT_ALICE);
+        appointmentList.setAppointment(APPT_ALICE, APPT_ALICE);
+        AppointmentList expectedList = new AppointmentList();
+        expectedList.add(APPT_ALICE);
+        assertEquals(expectedList, appointmentList);
+    }
+
+    @Test
+    public void setAppointment_editedAppointmentHasSameIdentity_success() {
+        appointmentList.add(APPT_ALICE);
+        Appointment editedAlice = new AppointmentBuilder(APPT_ALICE)
+                .withNotes("Changed notes")
+                .build();
+        appointmentList.setAppointment(APPT_ALICE, editedAlice);
+        AppointmentList expectedList = new AppointmentList();
+        expectedList.add(editedAlice);
+        assertEquals(expectedList, appointmentList);
+    }
+
+    @Test
+    public void setAppointment_editedAppointmentHasDifferentIdentity_success() {
+        appointmentList.add(APPT_ALICE);
+        appointmentList.setAppointment(APPT_ALICE, APPT_BENSON);
+        AppointmentList expectedList = new AppointmentList();
+        expectedList.add(APPT_BENSON);
+        assertEquals(expectedList, appointmentList);
+    }
+
+    @Test
+    public void setAppointment_editedAppointmentHasNonUniqueIdentity_throwsDuplicateAppointmentException() {
+        appointmentList.add(APPT_ALICE);
+        appointmentList.add(APPT_BENSON);
+        assertThrows(DuplicateAppointmentException.class, () -> appointmentList.setAppointment(APPT_ALICE, APPT_BENSON));
+    }
+
+    @Test
+    public void remove_nullAppointment_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> appointmentList.remove(null));
+    }
+
+    @Test
+    public void remove_appointmentDoesNotExist_throwsAppointmentNotFoundException() {
+        assertThrows(AppointmentNotFoundException.class, () -> appointmentList.remove(APPT_ALICE));
+    }
+
+    @Test
+    public void remove_existingAppointment_removesAppointment() {
+        appointmentList.add(APPT_ALICE);
+        appointmentList.remove(APPT_ALICE);
+        AppointmentList expectedList = new AppointmentList();
+        assertEquals(expectedList, appointmentList);
+    }
+
+    @Test
+    public void setAppointments_nullAppointmentList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> appointmentList.setAppointments((AppointmentList) null));
+    }
+
+    @Test
+    public void setAppointments_appointmentList_replacesOwnListWithProvidedAppointmentList() {
+        appointmentList.add(APPT_ALICE);
+        AppointmentList expectedList = new AppointmentList();
+        expectedList.add(APPT_BENSON);
+        appointmentList.setAppointments(expectedList);
+        assertEquals(expectedList, appointmentList);
+    }
+
+    @Test
+    public void setAppointments_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> appointmentList.setAppointments((List<Appointment>) null));
+    }
+
+    @Test
+    public void setAppointments_list_replacesOwnListWithProvidedList() {
+        appointmentList.add(APPT_ALICE);
+        List<Appointment> apptList = Collections.singletonList(APPT_BENSON);
+        appointmentList.setAppointments(apptList);
+        AppointmentList expectedList = new AppointmentList();
+        expectedList.add(APPT_BENSON);
+        assertEquals(expectedList, appointmentList);
+    }
+
+    @Test
+    public void setAppointments_listWithDuplicateAppointments_throwsDuplicateAppointmentException() {
+        List<Appointment> listWithDuplicateAppointments = Arrays.asList(APPT_ALICE, APPT_ALICE);
+        assertThrows(DuplicateAppointmentException.class, () -> appointmentList.setAppointments(listWithDuplicateAppointments));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+            -> appointmentList.asUnmodifiableObservableList().remove(0));
+    }
+
+    @Test
+    public void toStringMethod() {
+        assertEquals(appointmentList.asUnmodifiableObservableList().toString(), appointmentList.toString());
+    }
 }
---
+++ src/test/java/seedu/address/testutil/TypicalAppointments.java
@@ -0,0 +1,69 @@
+package seedu.address.testutil;
+
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.AddressBook;
+import seedu.address.model.person.IdentityNumber;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A utility class containing a list of {@code Appointment} objects to be used in tests.
+ */
+public class TypicalAppointments {
+
+    public static final Appointment APPT_ALICE = new AppointmentBuilder()
+            .withNotes("Routine checkup")
+            .withTime("01-12-2023 09:00")
+            .withPatientId(TypicalPersons.ALICE.getIdentityNumber())
+            .build();
+
+    public static final Appointment APPT_BENSON = new AppointmentBuilder()
+            .withNotes("Dental cleaning")
+            .withTime("02-12-2023 10:30")
+            .withPatientId(TypicalPersons.BENSON.getIdentityNumber())
+            .build();
+
+    public static final Appointment APPT_CARL = new AppointmentBuilder()
+            .withNotes("Consultation for flu symptoms")
+            .withTime("03-12-2023 11:15")
+            .withPatientId(TypicalPersons.CARL.getIdentityNumber())
+            .build();
+
+    public static final Appointment APPT_DANIEL = new AppointmentBuilder()
+            .withNotes("Follow-up appointment")
+            .withTime("04-12-2023 14:00")
+            .withPatientId(TypicalPersons.DANIEL.getIdentityNumber())
+            .build();
+
+    public static final Appointment APPT_ELLE = new AppointmentBuilder()
+            .withNotes("Vaccination")
+            .withTime("05-12-2023 15:45")
+            .withPatientId(TypicalPersons.ELLE.getIdentityNumber())
+            .build();
+
+    public static final Appointment APPT_FIONA = new AppointmentBuilder()
+            .withNotes("Blood test")
+            .withTime("06-12-2023 08:30")
+            .withPatientId(TypicalPersons.FIONA.getIdentityNumber())
+            .build();
+
+    public static final Appointment APPT_GEORGE = new AppointmentBuilder()
+            .withNotes("Annual physical exam")
+            .withTime("07-12-2023 13:00")
+            .withPatientId(TypicalPersons.GEORGE.getIdentityNumber())
+            .build();
+
+    private TypicalAppointments() {} // prevents instantiation
+
+    /**
+     * Returns a list of typical appointments.
+     */
+    public static List<Appointment> getTypicalAppointments() {
+        return new ArrayList<>(Arrays.asList(
+                APPT_ALICE, APPT_BENSON, APPT_CARL, APPT_DANIEL, APPT_ELLE, APPT_FIONA, APPT_GEORGE
+        ));
+    }
+}
+
