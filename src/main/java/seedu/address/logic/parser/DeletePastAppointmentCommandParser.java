732aa12a3c2dc9d0486a7a97cf8feee1b74a2c13
w3njii <wenjie.chong2018@gmail.com>
2025-10-29 13:04:39 +0800

    Add DeletePastAppointmentCommandParser

--- src/main/java/seedu/address/logic/parser/DeletePastAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeletePastAppointmentCommandParser.java
@@ -3,22 +3,23 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeletePastAppointmentCommand;
 import seedu.address.logic.commands.DeleteUpcomingAppointmentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DeleteUpcomingAppointmentCommandParser object
+ * Parses input arguments and creates a new DeletePastAppointmentCommand object
  */
-public class DeleteUpcomingAppointmentCommandParser implements Parser<DeleteUpcomingAppointmentCommand> {
+public class DeletePastAppointmentCommandParser implements Parser<DeletePastAppointmentCommand> {
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteUpcomingAppointmentCommandParser
-     * and returns a DeleteUpcomingAppointmentCommandParser object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeletePastAppointmentCommand
+     * and returns a DeletePastAppointmentCommand object for execution.
      * @throws ParseException if the user input does not conform to the expected format
      */
-    public DeleteUpcomingAppointmentCommand parse(String args) throws ParseException {
+    public DeletePastAppointmentCommand parse(String args) throws ParseException {
         try {
             Index index = ParserUtil.parseIndex(args);
-            return new DeleteUpcomingAppointmentCommand(index);
+            return new DeletePastAppointmentCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteUpcomingAppointmentCommand.MESSAGE_USAGE), pe);
