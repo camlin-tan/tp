f94489db4744ded45ae30404077c784cf389bd75
Soo42 <soojiayang@gmail.com>
2025-10-29 12:21:55 +0800

    Add testing for list command

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -14,11 +14,10 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
-    public static final String MESSAGE_WARNING = " \nNotes: Additional arguments detected. You may provide extra "
+    public static final String MESSAGE_WARNING = " \nNote: Additional arguments detected. You may provide extra "
             + "arguments, but they will be ignored.\n";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all persons in the address book."
-            + MESSAGE_WARNING + "Example: " + COMMAND_WORD + " extra arguments";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all persons in the address book.";
 
     private final boolean hasExtraArgs;
 
@@ -47,5 +46,19 @@ public class ListCommand extends Command {
         }
 
         return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof ListCommand)) {
+            return false;
+        }
+
+        ListCommand otherListCommand = (ListCommand) other;
+        return this.hasExtraArgs == otherListCommand.hasExtraArgs;
     }
 }
