8edf47fff71ea9af88bd360fa37b85383216d144
Si Yu Lew <156303901+bananapielearnsjava@users.noreply.github.com>
2025-10-30 11:28:54 +0800

    Debug AddCommandIntegrationTest

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -89,8 +89,7 @@ public class AddCommand extends Command {
         }
 
         model.addPerson(toAdd);
-        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)),
-                false, false, toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
     }
 
     @Override
